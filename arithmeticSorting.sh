d9f654d969aa1929a00c22fbcb06fd38d0735efa
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-03-31 18:33:38 +0530

    ADDED: Sort the result in descending order

--- arithmeticSorting.sh
+++ arithmeticSorting.sh
@@ -1,6 +1,22 @@
 #!/bin/bash -x
 
-echo "Enter three values :"
+#Function for descending sort
+
+function descendingSort(){
+   for (( i=1;i<=4;i++ ))
+   do
+      for (( j=1;j<=4;j++ ))
+      do
+         if [ ${array[$i]} -gt ${array[$j]} ]
+         then
+            temp=${array[$i]};
+            array[$i]=${array[$j]};
+            array[$j]=$temp;
+         fi
+      done
+   done
+}
+
 
 read -p "Enter the value of a :" Number1
 read -p "Enter the value of b :" Number2
@@ -13,6 +29,7 @@ ResultantArray[exp2]=$(($Number1*$Number2+$Number3));
 ResultantArray[exp3]=$(($Number3+$Number1/$Number2));
 ResultantArray[exp4]=$(($Number1%$Number2+$Number3));
 
+
 for (( i=1;i<=4;i++ ))
 do
 
@@ -20,3 +37,5 @@ do
 
 done
 
+descendingSort
+echo "${array[@]}"
