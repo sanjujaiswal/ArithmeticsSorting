ae02b848c3468408f9dea4ce57be7ba5cff9bb16
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-03-31 16:13:53 +0530

    ADDED: Taking three inputs a,b,c from user

--- arithmeticSorting.sh
+++ arithmeticSorting.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -x
 
+#taking thee inputs from user
+
 echo "Enter three values :"
+
 read -p "Enter the value of a :"Number1
 read -p "Enter the value of b :"Number2
 read -p "Enter the value of c :"Number3
