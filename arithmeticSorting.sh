a93e6114551fdf0205937590ce86511e26270756
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-03-31 18:06:38 +0530

    ADDED: Result stored in dictionary

--- arithmeticSorting.sh
+++ arithmeticSorting.sh
@@ -6,14 +6,19 @@ read -p "Enter the value of a :" Number1
 read -p "Enter the value of b :" Number2
 read -p "Enter the value of c :" Number3
 
+declare -a ResultantArray
+
 Result=$(($Number1+$Number2*$Number3))
-echo "Result of a+b*c is :$Result"
+ResultantArray[exp1]=$Result;
 
 Result2=$(($Number1*$Number2+$Number3))
-echo "Result of a*b+c is :$Result2"
+ResultantArray[exp2]=$Result2;
 
 Result3=$(($Number3+$Number1/$Number2))
-echo "Result of c+a/b is :$Result3"
+ResultantArray[exp3]=$Result3;
 
 Result4=$(($Number1%$Number2+$Number3))
-echo "Result of a%b+c is :$Result4"
+ResultantArray[exp4]=$Result4;
+
+echo "${!ResultantArray[@]}"
+echo "${ResultantArray[@]}"
